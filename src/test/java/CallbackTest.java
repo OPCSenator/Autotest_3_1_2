959925dce96b855fda0538dc3935367e315fa68b
OPCSenator <opc_senator4@mail.ru>
2023-02-18 21:30:57 +0300

    first commit

---
+++ src/test/java/CallbackTest.java
@@ -0,0 +1,31 @@
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+
+    class CallbackTest {
+        @Test
+        void firstTest() {
+            open("http://localhost:9999");
+            SelenideElement formName = $("[data-test-id=name]");
+            formName.$("[data-test-id=name] input").setValue("Иван Иванович Иванов");
+            SelenideElement formPhone = $("[data-test-id=phone");
+            formPhone.$("[data-test-id=phone] input").setValue("+01234567890");
+
+            $("[class = 'checkbox__box']").click();
+            $("[class = 'button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+
+            $(withText("Ваша заявка успешно отправлена!")).shouldBe(visible);
+            }
+    }
+
+
+
